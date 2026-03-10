a4d69c326ed901e9b4219fbea9828afd70664215
Florian Arndt <far@connext.de>
2026-03-10 14:10:52 +0100

    Add PdfViewerController for PDF document handling and integrate Syncfusion PDF libraries

--- Vereinsmanager.Server.Core/Program.cs
+++ Vereinsmanager.Server.Core/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHealthChecks();
+builder.Services.AddMemoryCache();
 
 var licenseKey = File.ReadAllText("syncfusion-license.txt");
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(licenseKey);
