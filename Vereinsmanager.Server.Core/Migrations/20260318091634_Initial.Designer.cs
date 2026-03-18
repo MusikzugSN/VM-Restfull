1076d0f82b7d5dd6ed3343030ed979928961f47b
Florian Arndt <far@connext.de>
2026-03-18 10:17:01 +0100

    Add Migrations folder and create ServerDatabaseContextModelSnapshot for EF Core

--- Vereinsmanager.Server.Core/Migrations/20260318091634_Initial.Designer.cs
+++ Vereinsmanager.Server.Core/Migrations/20260318091634_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Vereinsmanager.Database;
 namespace Vereinsmanager.Migrations
 {
     [DbContext(typeof(ServerDatabaseContext))]
-    [Migration("20260317112033_Initial")]
+    [Migration("20260318091634_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
