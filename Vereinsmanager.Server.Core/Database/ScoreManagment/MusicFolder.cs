6092cc467bd7f4aa27576289ace1a8da61c20f29
DominikB2003 <Dominik.Beneke@edu.bib.de>
2026-03-18 10:35:56 +0100

    ShowInMe gelöscht und ShowInMyArea angepasst

--- Vereinsmanager.Server.Core/Database/ScoreManagment/MusicFolder.cs
+++ Vereinsmanager.Server.Core/Database/ScoreManagment/MusicFolder.cs
@@ -17,12 +17,9 @@ public class MusicFolder : MetaData
     [Required]
     [MaxLength(24)]
     public required string Name { get; set; }
-
-    [Required] 
-    public bool ShowInMe { get; set; } = false;
     
     [Required]
-    public Boolean ShowInMyArea { get; set; }
+    public bool ShowInMyArea { get; set; }
     
     public ICollection<ScoreMusicFolder> ScoreMusicFolders { get; set; } = new List<ScoreMusicFolder>();
 }
